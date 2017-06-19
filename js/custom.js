04772aa800e313fc713dfec94ecce93c17821400
root <root@localhost.localdomain>
2017-06-19 18:31:09 +0530

    Fork Me added

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	var one, two, op, result;
+
+	//For calculation
 	$('.btn-group button').on('click', function() {
 		op = $(this).attr('class');
 		one = $('#one').val();
@@ -25,5 +27,13 @@ $(document).ready(function() {
 			result = 0;
 		}
 		$('#result').val(result);
+	});
+
+	//Click event for fork me
+	$('selector').action
+
+	jQuery('selector').action
+	$('.fork-me img').on('click', function() {
+		window.location.href = "http://github.com/RaajNadar/SimpleCalculator";
 	});
 });
